fce98fc143f2770c45c1352d407c30837ab9a6d8
Damien Beaufils <damien.beaufils@gmail.com>
2014-03-18 22:38:33 +0100

    Initial commit.
    Basic application with Grails 2.3.7 and Hibernate 4.
    Configured to use Maven (works with Maven 3.1.1).
    External datasource configuration for CloudBees continuous deployment.

---
+++ grails-app/conf/pointeuse-config.groovy
@@ -0,0 +1,11 @@
+environments {
+    production {
+        dataSource {
+            dbCreate = "create-drop"
+            driverClassName = "org.h2.Driver"
+            username = "sa"
+            password = ""
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+}
